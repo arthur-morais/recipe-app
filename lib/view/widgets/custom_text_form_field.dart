0e6d47e592fddbafacbd9aac48c23faf31acd4cf
Arthur <arthurmoraiis@hotmail.com>
2023-12-19 20:40:54 -0300

    refactoring custom text form field

--- lib/view/widgets/custom_text_form_field.dart
+++ lib/view/widgets/custom_text_form_field.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/constants/colors.dart';
+import 'package:recipes/constants/text_styles.dart';
 
 class CustomTextFormField extends StatefulWidget {
-  const CustomTextFormField({
+  CustomTextFormField({
     super.key,
     required this.fieldLabel,
     this.controller,
@@ -15,13 +17,25 @@ class CustomTextFormField extends StatefulWidget {
 }
 
 class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  final defaultBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: const BorderSide(color: AppColors.neutral30),
+  );
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      style: AppTextStyles.small14w400.apply(color: AppColors.neutral90),
       controller: widget.controller,
       decoration: InputDecoration(
-        label: Text(widget.fieldLabel),
+        filled: true,
+        fillColor: AppColors.white,
         floatingLabelBehavior: FloatingLabelBehavior.always,
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        hintText: widget.fieldLabel,
+        hintStyle: AppTextStyles.small14w400.apply(color: AppColors.neutral30),
+        border: defaultBorder,
       ),
     );
   }
