1d7cb443d61e32ed38a40313ef96b07ba49382af
Arthur <arthurmoraiis@hotmail.com>
2023-12-19 20:37:47 -0300

    adding new colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
   AppColors._();
+
+  static const Color primary = Color(0xffE23E3E);
+
+  static const Color error = Color(0xffEE1133);
+  static const Color lightError = Color(0xffFDE7EB);
+  static const Color success = Color(0xff31B057);
+  static const Color lightSuccess = Color(0xffCEECD7);
+
+  static const Color neutral20 = Color(0xffD9D9D9);
+  static const Color neutral30 = Color(0xffC1C1C1);
+  static const Color neutral50 = Color(0xff919191);
+  static const Color neutral90 = Color(0xff121212);
+
+  static const Color white = Color(0xffffffff);
 }
