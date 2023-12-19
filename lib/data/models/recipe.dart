11bd252a75280f8b8ed3a0aed4f7dc4c6e2dd9f4
Arthur <arthurmoraiis@hotmail.com>
2023-12-18 22:06:50 -0300

    creating recipe model

---
+++ lib/data/models/recipe.dart
@@ -0,0 +1,45 @@
+class RecipeModel {
+  final List instructions;
+  final String thumbnailUrl;
+  final String name;
+  final List ingredientsAndMeasurement;
+  final String description;
+
+  RecipeModel({
+    required this.instructions,
+    required this.thumbnailUrl,
+    required this.name,
+    required this.ingredientsAndMeasurement,
+    required this.description,
+  });
+
+  factory RecipeModel.fromJson(dynamic json) {
+    List<String> instructionsList = (json['instructions'] as List)
+        .map((item) => item['display_text'].toString())
+        .toList();
+
+    List<String> ingredientsAndMeasurementRawText =
+        (json['sections'][0]['components'] as List)
+            .map((item) => item['raw_text'].toString())
+            .toList();
+
+    return RecipeModel(
+      instructions: instructionsList,
+      thumbnailUrl: json['thumbnail_url'] as String,
+      name: json['name'] as String,
+      ingredientsAndMeasurement: ingredientsAndMeasurementRawText,
+      description: json['description'] as String,
+    );
+  }
+
+  static List<RecipeModel> recipesFromSnapshot(List snapshot) {
+    return snapshot.map((data) {
+      return RecipeModel.fromJson(data);
+    }).toList();
+  }
+
+  @override
+  String toString() {
+    return 'Recipe {name: $name, description: $description, ingredients and measurement: $ingredientsAndMeasurement, instructions: $instructions, thumbnailUrl: $thumbnailUrl},';
+  }
+}
