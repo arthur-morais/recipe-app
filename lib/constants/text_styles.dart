b593bf9a7c33cdc441ddd08cd634e62d687ec78c
Arthur <arthurmoraiis@hotmail.com>
2023-12-21 18:38:05 -0300

    add new text styles

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -9,6 +9,30 @@ class AppTextStyles {
     fontFamily: 'Poppins',
   );
 
+  static const TextStyle medium32w700 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle medium24w700 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle small16w500 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle small16w700 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Poppins',
+  );
+
   static const TextStyle small14w400 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w400,
