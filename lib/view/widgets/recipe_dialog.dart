b73bfdcd72c7b40592b59d4c8b5fe1130a48178b
Arthur <arthurmoraiis@hotmail.com>
2023-12-21 18:45:45 -0300

    refactor recipe dialog widget

--- lib/view/widgets/recipe_dialog.dart
+++ lib/view/widgets/recipe_dialog.dart
@@ -1,93 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/constants/colors.dart';
+import 'package:recipes/constants/text_styles.dart';
 
-Future<void> recipeDialog(
-  BuildContext context, {
-  required List mealInstructions,
-  required List mealIngredientsAndMeasurements,
-}) {
-  return showDialog<void>(
-    context: context,
-    builder: (context) {
-      final double screenHeight = MediaQuery.of(context).size.height;
+class RecipeDialog extends StatefulWidget {
+  const RecipeDialog({
+    super.key,
+    required this.mealInstructions,
+    required this.mealIngredientsAndMeasurements,
+    required this.mealName,
+  });
 
-      return Dialog(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Bistek'),
+  final List mealInstructions;
+  final List mealIngredientsAndMeasurements;
+  final String mealName;
+
+  @override
+  State<RecipeDialog> createState() => _RecipeDialogState();
+}
+
+class _RecipeDialogState extends State<RecipeDialog> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Dialog(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          titleSpacing: 0,
+          automaticallyImplyLeading: false,
+          title: Text(
+            widget.mealName,
+            style: AppTextStyles.small16w700,
           ),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 16,
-            ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 16,
+          ),
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text('Ingredients'),
+                Text(
+                  'Ingredients and Measurements',
+                  style: AppTextStyles.medium24w700
+                      .apply(color: AppColors.neutral90),
+                ),
                 SizedBox(
                   height: screenHeight * 0.015,
+                ),
+                Text(
+                  widget.mealIngredientsAndMeasurements.toString(),
+                  style: AppTextStyles.small14w400
+                      .apply(color: AppColors.neutral90),
                 ),
                 SizedBox(
                   height: screenHeight * 0.025,
                 ),
-                const Text('Instructions'),
+                Text(
+                  'Instructions',
+                  style: AppTextStyles.medium24w700
+                      .apply(color: AppColors.neutral90),
+                ),
                 SizedBox(
                   height: screenHeight * 0.015,
+                ),
+                Text(
+                  widget.mealInstructions.toString(),
+                  style: AppTextStyles.small14w400
+                      .apply(color: AppColors.neutral90),
                 ),
               ],
             ),
           ),
-          bottomNavigationBar: Container(
-            height: 50,
-            decoration: const BoxDecoration(color: Colors.blue),
-            child: Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.purple),
-                      iconColor: MaterialStateProperty.all<Color>(Colors.white),
-                    ),
-                    child: const Row(
-                      children: [
-                        Icon(Icons.delete),
-                        Text(
-                          'EDIT',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.05,
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.red),
-                      iconColor: MaterialStateProperty.all<Color>(Colors.white),
-                    ),
-                    child: const Row(
-                      children: [
-                        Icon(Icons.delete),
-                        Text(
-                          'DELETE',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+        ),
+        bottomNavigationBar: Container(
+          height: 50,
+          decoration: const BoxDecoration(color: AppColors.primary),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                padding: EdgeInsets.only(left: screenWidth * 0.05),
+                color: AppColors.white,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios),
               ),
-            ),
+            ],
           ),
         ),
-      );
-    },
-  );
+      ),
+    );
+  }
 }
