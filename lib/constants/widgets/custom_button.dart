85ac37ebe94b8d7d9ab9e19fdeb7b3b01b066e44
Arthur <arthurmoraiis@hotmail.com>
2023-12-22 17:28:54 -0300

    improving custombutton widget

--- lib/constants/widgets/custom_button.dart
+++ lib/constants/widgets/custom_button.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/constants/text_styles.dart';
 
 import '../colors.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({
     super.key,
+    this.icon,
     required this.buttonText,
-    required this.icon,
     required this.buttonColor,
+    required this.textColor,
+    this.onPressed,
   });
 
   final String buttonText;
   final Color buttonColor;
-  final IconData icon;
+  final Color textColor;
+  final IconData? icon;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
 
     return TextButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
         iconColor: MaterialStateProperty.all<Color>(AppColors.white),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(icon),
+          icon != null ? Icon(icon) : Container(),
           SizedBox(
             width: screenWidth * 0.01,
           ),
           Text(
             buttonText,
-            style: const TextStyle(color: AppColors.white),
+            style: AppTextStyles.small16w700.apply(color: textColor),
           ),
         ],
       ),
