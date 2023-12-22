80cb51bf785e35c5a036ff1d97226c21da0eac53
Arthur <arthurmoraiis@hotmail.com>
2023-12-22 17:29:17 -0300

    improving customtextformfield widget

--- lib/constants/widgets/custom_text_form_field.dart
+++ lib/constants/widgets/custom_text_form_field.dart
@@ -3,14 +3,16 @@ import 'package:recipes/constants/colors.dart';
 import 'package:recipes/constants/text_styles.dart';
 
 class CustomTextFormField extends StatefulWidget {
-  CustomTextFormField({
+  const CustomTextFormField({
     super.key,
-    required this.fieldLabel,
+    this.fieldLabel,
     this.controller,
+    this.validator,
   });
 
   final TextEditingController? controller;
-  final String fieldLabel;
+  final String? fieldLabel;
+  final FormFieldValidator<String>? validator;
 
   @override
   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
@@ -25,6 +27,8 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      textInputAction: TextInputAction.next,
+      validator: widget.validator,
       style: AppTextStyles.small14w400.apply(color: AppColors.neutral90),
       controller: widget.controller,
       decoration: InputDecoration(
@@ -36,6 +40,9 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
         hintText: widget.fieldLabel,
         hintStyle: AppTextStyles.small14w400.apply(color: AppColors.neutral30),
         border: defaultBorder,
+        errorBorder: defaultBorder.copyWith(
+            borderSide: const BorderSide(color: AppColors.error)),
+        errorStyle: AppTextStyles.small12w500,
       ),
     );
   }
