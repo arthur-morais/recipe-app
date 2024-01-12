e2d53785af46e980aea1c8907eb464a964153b2f
Arthur <arthurmoraiis@hotmail.com>
2024-01-12 15:46:13 -0300

    add style to defaultsnackbar

--- lib/constants/snackbars.dart
+++ lib/constants/snackbars.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/constants/colors.dart';
+import 'package:recipes/constants/text_styles.dart';
 
 class AppSnackbars {
   AppSnackbars._();
 
   static SnackBar defaultSnackbar(String content) {
     return SnackBar(
-      content: Text(content),
+      content: Text(content, style: AppTextStyles.small16w700.apply(color: AppColors.white)),
+      backgroundColor: AppColors.primary,
     );
   }
 }
